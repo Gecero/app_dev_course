16b54e3d353cdf55559486dc2d7fd745689cc184
Gecero <27288418+Gecero@users.noreply.github.com>
2019-01-29 22:18:24 +0100

    homework 2 done

---
+++ homework2/Assets/Cube.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("w"))
+            transform.Translate(new Vector3(0.0f, 200.0f * Time.deltaTime, 0.0f));
+        if (Input.GetKey("s"))
+            transform.Translate(new Vector3(0.0f, -2.0f * Time.deltaTime, 0.0f));
+
+    }
+
+    // called once at the start of the collision
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        Debug.Log("collision!!1einself");
+    }
+
+    // while the collision
+    void OnCollisionStay(Collision collisionInfo)
+    {
+        Debug.Log("FKN COLLISION DETECTED OMG :|D~");
+    }
+
+    // gets called if the collision ends
+    void OnCollisionExit(Collision collisionInfo)
+    {
+        //Debug.Log("no collision anymore d(;d_;)b");
+    }
+
+}
