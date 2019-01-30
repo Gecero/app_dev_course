5258a88f0768f4603a1a212eda73c04dcdf9f720
Gecero <27288418+Gecero@users.noreply.github.com>
2019-01-30 20:22:08 +0100

    changed some stuff because of the trello.com quest system

---
+++ homework2/Assets/_MA/Scripts/VerticalMover.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalMover : MonoBehaviour
+{
+
+	private Rigidbody rb;
+	public float moveSpeed = 175.0f;
+
+	// Use this for initialization
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKey("w"))
+		{
+			rb.isKinematic = true;
+			transform.Translate(new Vector3(0.0f, moveSpeed * Time.deltaTime, 0.0f));
+			rb.isKinematic = false;
+		}
+		if (Input.GetKey("s"))
+		{
+			rb.isKinematic = true;
+			transform.Translate(new Vector3(0.0f, -moveSpeed * Time.deltaTime, 0.0f));
+			rb.isKinematic = false;
+		}
+
+	}
+
+	// called once at the start of the collision
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+		Debug.Log("collision!!1einself");
+	}
+
+	// while the collision
+	void OnCollisionStay(Collision collisionInfo)
+	{
+		Debug.Log("FKN COLLISION DETECTED OMG :|D~");
+	}
+
+	// gets called if the collision ends
+	void OnCollisionExit(Collision collisionInfo)
+	{
+		//Debug.Log("no collision anymore d(;d_;)b");
+	}
+
+}
